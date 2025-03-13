98da55e8fd1f8d4a9e90070e9d0f90ba1f8c54fd
24.11.6236 <arielardhi12345@students.amikom.ac.id>
2025-03-13 11:29:18 +0700

    Penambahan fungsi penjumlahan dan pengurangan

--- calculator.cs
+++ calculator.cs
@@ -1,7 +1,25 @@
 class Calculator
 {
 
-static void Main(string[args)
+static void Main(string[]args)
 {
+	int a = 10;
+	int b = 6;
+	
+	Console.Writeline("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+	Console.Writeline("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+	
+	Console.Writeline("\nTekan sembarang key untuk keluar');
+	Console.readkey();
+}
+
+static int penambahan(int a, int b)
+{
+	return a+ b;
+}
+static int pengurangan(int a, int b)
+{
+	return a- b;
 }
 }
+
